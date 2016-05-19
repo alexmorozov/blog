1d8c74246e829563cd8eef0e74debb3de199a825
Alex Morozov <inductor2000@mail.ru>
2016-05-19 11:57:11 +0300

    Added FB icon and fixed SO spelling so that the icon appears.

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,9 +31,10 @@ TYPOGRIFY = True
 # Social widget
 SOCIAL = (
     ('linkedin', 'https://linkedin.com/in/djangoengineer'),
-    ('stackoverflow', 'http://stackoverflow.com/cv/django-developer'),
+    ('stack-overflow', 'http://stackoverflow.com/cv/django-developer'),
     ('twitter', 'https://twitter.com/alexmorozov'),
     ('github', 'https://github.com/alexmorozov'),
+    ('facebook', 'https://facebook.com/theveryalexmorozov'),
     ('rss', '%s/%s' % (SITEURL, FEED_ALL_ATOM)),
 )
 
